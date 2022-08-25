522f524addb5bdc83ebee95a16a9b096f18eb6c6
Earl Anthony Gamboa <111488396+Tsukkei@users.noreply.github.com>
2022-08-25 14:53:33 +0800

    Create App.java

---
+++ student_class/App.java
@@ -0,0 +1,59 @@
+public class App {
+    public static void main(String[] args) {
+
+        Character Swordsman = new Character();
+
+        Swordsman.strength = 10;
+        Swordsman.agility = 10;
+        Swordsman.intelligence = 9;
+        Swordsman.name = "Vista";
+        Swordsman.sayMyName();
+        Swordsman.sayMyStrength();
+        Swordsman.sayMyAgility();
+        Swordsman.sayMyIntelligence();
+
+        Character Cook = new Character();
+
+        Cook.strength = 9;
+        Cook.agility = 9;
+        Cook.intelligence = 9;
+        Cook.name = "Oden";
+        Cook.sayMyName();
+        Cook.sayMyStrength();
+        Cook.sayMyAgility();
+        Cook.sayMyIntelligence();
+
+        Character Captain = new Character();
+
+        Captain.strength = 9;
+        Captain.agility = 10;
+        Captain.intelligence = 10;
+        Captain.name = "Edward Newgate";
+        Captain.sayMyName();
+        Captain.sayMyStrength();
+        Captain.sayMyAgility();
+        Captain.sayMyIntelligence();
+
+        Character Sniper = new Character();
+
+        Sniper.strength = 6;
+        Sniper.agility = 9;
+        Sniper.intelligence = 10;
+        Sniper.name = "Izzo";
+        Sniper.sayMyName();
+        Sniper.sayMyStrength();
+        Sniper.sayMyAgility();
+        Sniper.sayMyIntelligence();
+
+        Character Navigator = new Character();
+
+        Navigator.strength = 2;
+        Navigator.agility = 7;
+        Navigator.intelligence = 10;
+        Navigator.name = "ACE";
+        Navigator.sayMyName();
+        Navigator.sayMyStrength();
+        Navigator.sayMyAgility();
+        Navigator.sayMyIntelligence();
+    }
+}
